61780d985bec886de79e66e33c02f14fe65847d2
jamesparker01 <30592054+jamesparker01@users.noreply.github.com>
2020-02-02 17:23:59 +0000

    remove comment

--- CosmosIntegrationTests/Repository.cs
+++ CosmosIntegrationTests/Repository.cs
@@ -12,7 +12,6 @@ namespace CosmosIntegrationTests
             //https://docs.microsoft.com/en-us/azure/cosmos-db/local-emulator
             //https://github.com/Azure/azure-cosmos-dotnet-v3/blob/master/Microsoft.Azure.Cosmos.Samples/Usage/ItemManagement/Program.cs
             var client = new CosmosClient("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-            //client.CreateDatabaseIfNotExistsAsync
             _container = client.GetContainer("databaseId", "containerId");
         }
 
